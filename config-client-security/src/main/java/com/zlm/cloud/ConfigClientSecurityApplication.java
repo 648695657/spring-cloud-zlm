24edc448fd51b1513c23a8de7d95856941db944d
648695657 <648695657@qq.com>
2019-10-21 17:38:01 +0800

    springcloud 学习

---
+++ config-client-security/src/main/java/com/zlm/cloud/ConfigClientSecurityApplication.java
@@ -0,0 +1,11 @@
+package com.zlm.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientSecurityApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigClientSecurityApplication.class, args);
+  }
+}
